da2577f118759bfcbd94a1da618e9d7539563cde
MarK <marcosmmc1402@gmail.com>
2025-08-23 09:15:52 -0500

    version enlazada

---
+++ script.js
@@ -0,0 +1,58 @@
+const inputNombre = document.getElementById("nombreInput"); 
+const btnAdicionar = document.getElementById("btnAdicionar");
+const btnSortear = document.getElementById("btnSortear");
+const listaNombres = document.getElementById("listaNombres");
+const resultado = document.getElementById("resultado");
+
+let participantes = [];
+
+//Función para adicionar un nombre
+btnAdicionar.addEventListener("click", () => {
+  const nombre = inputNombre.value.trim();
+
+  if (nombre === "") {
+    alert("⚠️ Escribe un nombre válido.");
+    return;
+  }
+
+  if (participantes.includes(nombre)) {
+    alert("⚠️ Ese nombre ya está en la lista.");
+    return;
+  }
+
+  participantes.push(nombre);
+
+  const li = document.createElement("li");
+  li.textContent = nombre;
+  listaNombres.appendChild(li);
+
+  inputNombre.value = "";
+  inputNombre.focus();
+});
+
+//Función para sortear un amigo
+btnSortear.addEventListener("click", () => {
+  if (participantes.length < 2) {
+    alert("⚠️ Se necesitan al menos 2 participantes para sortear.");
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * participantes.length);
+  const ganador = participantes[randomIndex];
+
+  mostrarResultado(`El amigo secreto es: ${ganador}...?`);
+});
+
+//cursor
+function mostrarResultado(texto) {
+  resultado.textContent = texto;
+
+  resultado.style.animation = "none";
+  resultado.offsetHeight; // Reinicia animación
+  resultado.style.animation = null;
+
+  resultado.style.width = "0";
+  resultado.style.animation =
+    "typing 3s steps(30, end) forwards, blinkCursor 0.7s infinite";
+}
+
